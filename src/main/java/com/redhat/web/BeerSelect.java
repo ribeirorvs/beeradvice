99f7926e19f1fc28947a53a45be5cd38a2730e2f
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-02-27 02:04:01 -0300

    Create the JSP and Servlet BeerSelect

--- src/main/java/com/redhat/web/BeerSelect.java
+++ src/main/java/com/redhat/web/BeerSelect.java
@@ -3,10 +3,31 @@
  */
 package com.redhat.web;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.redhat.model.BeerExpert;
+
 /**
  * @author roribeir
  *
  */
-public class BeerSelect {
-
+public class BeerSelect extends HttpServlet{
+	public void doPost(HttpServletRequest request, 
+					HttpServletResponse response)
+					throws IOException, ServletException{
+		String c = request.getParameter("color");
+		BeerExpert be = new BeerExpert();
+		List result = be.getBrands(c);
+		
+		request.setAttribute("styles", result);
+		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+		view.forward(request, response);
+	}
 }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -1,12 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.*" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Sample JSP</title>
 </head>
 <body>
-
+	<%
+		List styles = (List) request.getAttribute("styles");
+		Iterator it = styles.iterator();
+		while(it.hasNext()){
+			out.print("<br>Try: " + it.next());
+		}
+	%>
 </body>
 </html>
